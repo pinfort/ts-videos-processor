3d9cf3627e0217e663c2f642c911c42db25b5434
pinfort <ptg@nijitei.com>
2021-10-21 22:51:24 +0900

    update ddl and change main

--- ddl/create_database.sql
+++ ddl/create_database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `executed_file` (
  `id` bigint(20) NOT NULL AUTO_INCREMENT,
- `FILE` text COLLATE utf8mb4_bin NOT NULL,
+ `file` text COLLATE utf8mb4_bin NOT NULL,
  `drops` int(11) NOT NULL,
  `size` bigint(20) NOT NULL,
  `recorded_at` datetime NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `executed_file` (
  `channelName` text COLLATE utf8mb4_bin NOT NULL,
  `duration` double NOT NULL,
  PRIMARY KEY (`id`),
- UNIQUE KEY `FILE` (`FILE`) USING HASH
+ UNIQUE KEY `file` (`file`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
 
 CREATE TABLE `splitted_file` (
--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from main.command.tsSplitter import TsSplitter
 from main.command.amatsukazeAddTask import AmatsukazeAddTask
 
 def processPath(path: Path):
+    dropCheck = DropChk()
+    tsSplitter = TsSplitter()
+    amatsukazeAddTask = AmatsukazeAddTask()
     print(path)
     files: Iterable[Path]
 
     if(not path.exists):
         print("path not exist")
         sys.exit(1)
-    
+
     if(path.is_dir()):
         print("given path is directory")
         files = path.glob("*.m2ts")
@@ -28,10 +31,9 @@ def processPath(path: Path):
         tsSplitter.tsSplitter(file)
         amatsukazeAddTask.amatsukaze(file)
 
-if __name__ == "__main__":
-    dropCheck = DropChk()
-    tsSplitter = TsSplitter()
-    amatsukazeAddTask = AmatsukazeAddTask()
-
+def main():
     for input in sys.argv[1:]:
         processPath(Path(input))
+
+if __name__ == "__main__":
+    main()
