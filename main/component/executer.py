68f966e88b93dbccfaad11d74ea95afa72a811f5
pinfort <ptg@nijitei.com>
2021-10-29 18:33:02 +0900

    fix log encode

--- main/component/executer.py
+++ main/component/executer.py
@@ -6,8 +6,8 @@ def executeCommand(commands: list[str]) -> int:
     logger.debug(f"""executing command: {commands}""")
     result: subprocess.CompletedProcess = subprocess.run(commands, capture_output=True)
     if result.stdout:
-        logger.info("\n" + result.stdout.decode("shift_jis"))
+        logger.info("\n" + result.stdout.decode("cp932"))
     if result.stderr:
-        logger.warn("\n" + result.stderr.decode("shift_jis"))
+        logger.warn("\n" + result.stderr.decode("cp932"))
     logger.debug(f"""command executing finished: {commands}""")
     return result.returncode
