99f696efe3e2207aaf55c7a0a9696dd6425c4692
pinfort <ptg@nijitei.com>
2022-01-08 10:07:27 +0900

    catch exception

--- tsVideosProcessor.py
+++ tsVideosProcessor.py
@@ -2,7 +2,7 @@ import sys
 import json
 from pathlib import Path
 from typing import Iterable
-from logging import config, getLogger
+from logging import Logger, config, getLogger
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -15,37 +15,55 @@ class TsVideosProcessor:
     """
     全処理をまとめるメインクラス
     """
-    def processPath(self, path: Path):
+    logger: Logger
+    dropChk: DropChk
+    tsSplitter: TsSplitter
+    amatsukazeAddTask: AmatsukazeAddTask
+    compressAndSave: CompressAndSave
+
+    def __init__(self) -> None:
         dotenv_path = join(dirname(__file__), '.env')
         load_dotenv(dotenv_path)
 
         with open("log_config.json", 'r') as f:
             config.dictConfig(json.load(f))
-        logger = getLogger(__name__)
-        logger.info("processing path started.")
-        dropCheck = DropChk()
-        tsSplitter = TsSplitter()
-        amatsukazeAddTask = AmatsukazeAddTask()
-        compressAndSave = CompressAndSave()
-        logger.info(path)
+        self.logger = getLogger(__name__)
+
+        self.dropCheck = DropChk()
+        self.tsSplitter = TsSplitter()
+        self.amatsukazeAddTask = AmatsukazeAddTask()
+        self.compressAndSave = CompressAndSave()
+
+    def processPath(self, path: Path):
+        self.logger.info("processing path started.")
+        self.logger.info(path)
         files: Iterable[Path]
 
         if(not path.exists):
-            logger.error(f"""path not exist path:{str(path)}""")
+            self.logger.error(f"""path not exist path:{str(path)}""")
             sys.exit(1)
 
         if(path.is_dir()):
-            logger.info("given path is directory")
+            self.logger.info("given path is directory")
             files = path.glob("*.m2ts")
         else:
-            logger.info("given path is file")
+            self.logger.info("given path is file")
             files = list()
             files.append(path)
 
         for file in files:
-            logger.info(f"processing file:{file}")
-            dropCheck.dropChk(file)
-            splittedFile = tsSplitter.tsSplitter(file)
-            compressAndSave.execute(splittedFile)
-            amatsukazeAddTask.amatsukaze(splittedFile)
-        logger.info("processing path finished.")
+            try:
+                self.processFile(file)
+            except Exception as e:
+                self.logger.error("processing file failed. reason:" + str(e))
+        self.logger.info("processing path finished.")
+
+    def processFile(self, file: Path):
+        """
+        一ファイルごとに処理を行います
+        """
+        self.logger.info(f"processing file:{file}")
+        self.dropCheck.dropChk(file)
+        splittedFile = self.tsSplitter.tsSplitter(file)
+        self.compressAndSave.execute(splittedFile)
+        self.amatsukazeAddTask.amatsukaze(splittedFile)
